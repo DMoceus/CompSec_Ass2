e65c2650d54fd5d2c428764b1d1ff504c001835e
dmorris4 <dmorris4@binghamton.edu>
2015-03-19 14:13:51 -0400

    First Commit

---
+++ fscrypt.cpp
@@ -0,0 +1,8 @@
+#include "fscrypt.h"
+
+void *fs_encrypt(void *plaintext, int bufsize, char *keystr, int *resultlen){
+	
+}
+void *fs_decrypt(void *ciphertext, int bufsize, char *keystr, int *resultlen){
+	
+}
